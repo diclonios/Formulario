8c4403d338c9599ed512aa1fbc63a1c3a0258755
diclonios <69858398+diclonios@users.noreply.github.com>
2023-07-31 11:57:36 -0300

    Testando formulario

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+  $dbhost ="Localhost";
+  $dbUsername = "root";
+  $dbPassword = "";
+  $dbName = "usuarios";
+
+  $conexao = new mysqli($dbhost, $dbUsername, $dbPassword, $dbName);
+
+  //if($conexao->connect_errno)
+  //{
+	//echo "Erro";
+  //}
+  //else
+  //{
+	//echo "conexão efetuada com sucesso";
+  //}
+	
+?>
+
